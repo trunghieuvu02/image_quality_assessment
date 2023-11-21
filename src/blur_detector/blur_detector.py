f282f77200d2157ccca74f581913cc0fcce19f1a
trunghieuvu02 <mrtrunghieu139@gmail.com>
2023-11-21 12:30:46 +0000

    [ADD] Blur detector - base version

--- src/blur_detector/blur_detector.py
+++ src/blur_detector/blur_detector.py
@@ -1,3 +1,30 @@
 import cv2
 import os
 
+
+def detect_blur_spot(image_path, threshold):
+    # Read the image
+    image = cv2.imread(image_path)
+
+    # Convert image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Laplacian filter for edge detection
+    laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+
+    # Calculate maximum intensity and variance
+    laplacian_variance = laplacian.var()
+
+    # Initialize result variables
+    blur_text = "Not Blurry"
+
+    # Check blur condition based on variance of Laplacian image
+    if laplacian_variance < threshold:
+        blur_text = "Blurry"
+
+    # Add labels to the image
+    cv2.putText(image, "{}: {:.2f}".format(blur_text, laplacian_variance), (15, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+
+    # Display the image
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
--- src/main.py
+++ src/main.py
@@ -2,15 +2,18 @@ import os
 import sys
 import logging
 from configs import Parameters
+from blur_detector.blur_detector import detect_blur_spot
 
 logging.basicConfig(format='%(asctime)s | %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.info("Loading packages ...")
 
 
-def main():
-    pass
+def main(image_path):
+    detect_blur_spot(image_path, 350)
 
 
 if __name__ == "__main__":
-    args = Parameters().parser()
+    image_path = "/home/ktp_user/Documents/Github_repo/image_quality_assessment/datasets/blurry/img_10.png"
+    main(image_path)
+
